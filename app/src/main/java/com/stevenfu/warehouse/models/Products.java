888cae03b0d65bed6800f24efbee87b0526bae31
Steven Fu <sfu@synvata.com>
2016-12-01 16:58:27 +0800

    change format of products.

--- app/src/main/java/com/stevenfu/warehouse/models/Products.java
+++ app/src/main/java/com/stevenfu/warehouse/models/Products.java
@@ -4,4 +4,16 @@ package com.stevenfu.warehouse.models;
  * Created by Steven Fu on 12/01/2016.
  */
 
-public class Products extends Entity{public Products(){ } public int Id;   public String Name;   public String Specification;   public String Unit;   public double Width;   public double Height;   public double Length;   public String Brand;   public String Barcode;   public double Price;  }
+public class Products extends Entity{
+        public Products(){ }
+        public int Id;
+        public String Name;
+        public String Specification;
+        public String Unit;
+        public double Width;
+        public double Height;
+        public double Length;
+        public String Brand;
+        public String Barcode;
+        public double Price;
+}
